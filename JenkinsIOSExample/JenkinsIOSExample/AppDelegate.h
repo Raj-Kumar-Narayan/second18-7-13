6da804ba2fd1f3050eba2503db6dd75f225ffae9
Raj-LPU <er.rajnarayan@gmail.com>
2013-07-18 10:29:50 -0700

    Jenkins
    second test

---
+++ JenkinsIOSExample/JenkinsIOSExample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  JenkinsIOSExample
+//
+//  Created by admin on 12/07/13.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TestFlight.h"
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ JenkinsIOSExample/JenkinsIOSExampleTests/JenkinsIOSExampleTests.h
@@ -0,0 +1,13 @@
+//
+//  JenkinsIOSExampleTests.h
+//  JenkinsIOSExampleTests
+//
+//  Created by admin on 12/07/13.
+//  Copyright (c) 2013 admin. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JenkinsIOSExampleTests : SenTestCase
+
+@end
